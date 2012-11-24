ab187f0e775d2ca275643aff66eeaee322a9a31f
Fedor Indutny <fedor.indutny@gmail.com>
2012-11-24 22:16:20 +0400

    field: player and more

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,9 @@
   // Load all images first
   var images = {
         block: '/img/block.png',
-        grass: '/img/grass_block.png'
+        grass: '/img/grass_block.png',
+        player: '/img/player.png',
+        'player-flying': '/img/player-flying.png'
       },
       imageMap = {};
 
@@ -36,14 +38,46 @@
   function onImages() {
     requirejs(['ifield'], function(ifield) {
       var field = ifield.create(document.getElementById('field'), imageMap);
-      field.setCenter(0, 0);
+      field.setCenter(0, 0, -1);
 
-      for (var x = -25; x < 25; x++) {
-        for (var y = -25; y < 25; y++) {
+      var w = 25;
+      for (var x = -w; x < w; x++) {
+        for (var y = -w; y < w; y++) {
           var k = Math.random();
-          field.addBlock(ifield.createBlock(x, y, k > 0.8 ? 1 : 0, 'grass'));
+          field.add(ifield.createBlock(x, y, 0, k > 0.5 ? 'grass' : 'block'));
         }
       }
+
+      for (var x = -w; x < w; x++) {
+        field.add(ifield.createBlock(x, -2, -1, 'block'));
+        field.add(ifield.createBlock(x, -2, -2, 'block'));
+        field.add(ifield.createBlock(x, -2, -3, 'block'));
+      }
+
+      var player = ifield.createPlayer(0, 0, -1);
+      field.add(player);
+
+      window.addEventListener('keydown', function(e) {
+        var code = e.keyCode;
+
+        function cb() {
+          field.setCenter(player.x, player.y, player.z);
+        }
+
+        if (code === 37) {
+          player.move(-1, 0, 0, cb);
+        } else if (code === 39) {
+          player.move(1, 0, 0, cb);
+        } else if (code == 38) {
+          player.move(0, -1, 0, cb);
+        } else if (code == 40) {
+          player.move(0, 1, 0,cb );
+        } else {
+          return;
+        }
+
+        e.preventDefault();
+      }, true);
 
       // Resize field with window
       function onresize() {
--- public/js/ifield.js
+++ public/js/ifield.js
@@ -1,7 +1,18 @@
 define(function() {
   var exports = {};
 
-  // Isometric field prototype, responsible for rendering all blocks on the
+  function inherits(a, b) {
+    a.prototype = Object.create(b.prototype, {
+      constructor: {
+        value: a,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  };
+
+  // Isometric field prototype, responsible for rendering all items on the
   // screen.
   function IField(canvas, images) {
     this.canvas = canvas;
@@ -17,7 +28,7 @@ define(function() {
     this.cellWidth = 64;
     this.cellHeight = 32;
 
-    this.blocks = [];
+    this.items = [];
     this.center = null;
 
     this.s2 = Math.sqrt(2);
@@ -42,63 +53,251 @@ define(function() {
 
   IField.prototype.render = function render() {
     this.ctx.save();
-    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+    this.ctx.restore();
 
-    var center = this.project(this.center.x, this.center.y, 0);
+    this.ctx.save();
+
+    var center = this.project(this.center.x, this.center.y, this.center.z);
     var cx = Math.round(this.width / 2 - center.x);
     var cy = Math.round(this.height / 2 - center.y);
+
     this.ctx.translate(cx, cy);
 
-    this.blocks.forEach(function(block) {
-      this.ctx.save();
-      block.render(this.ctx, this);
-      this.ctx.restore();
-    }, this);
+    this.sortItems();
+    this.renderItems();
 
     this.ctx.restore();
+  };
+
+  IField.prototype.sortItems = function sortItems() {
+    for (var i = 0; i < this.items.length - 1; i++) {
+      var current = this.items[i],
+          next = this.items[i + 1];
+
+      if (Item.compare(current, next) > 0) {
+        break;
+      }
+    }
+
+    if (i !== this.items.length - 1) {
+      this.items.sort(Block.compare);
+    }
+  };
+
+  IField.prototype.renderItems = function renderItems() {
+    var firstZ = (this.items[0] && this.items[0].z) | 0,
+        lastZ = (this.items[this.items.length - 1] &&
+                 this.items[this.items.length - 1].z) | 0;
+
+    if (firstZ < this.center.z) {
+      firstZ = this.center.z;
+    }
+
+    for (var i = 0; i < this.items.length; i++) {
+      var block = this.items[i],
+          z = block.z |  0;
+
+      // Maximum visible depth is limited
+      if (z - this.center.z  >= 4) continue;
+
+      this.ctx.save();
+      if (this.center.z - z > 0) {
+        // Items above the player transparent
+        this.ctx.globalAlpha = 0.2;
+      } else if (this.center.z === z) {
+        // Items above the player transparent
+      } else if (firstZ != z) {
+        // Apply shadow
+        this.ctx.save();
+        this.ctx.fillStyle = 'rgba(0,0,0,0.4)';
+        this.ctx.fillRect(-this.width / 2, -this.height / 2,
+                          this.width, this.height);
+        this.ctx.restore();
+        firstZ = z;
+      }
+
+      block.render(this.ctx);
+      this.ctx.restore();
+    }
   };
 
   IField.prototype.setCenter = function setCenter(x, y, z) {
     this.center = { x: x, y: y, z: z};
   };
 
-  IField.prototype.addBlock = function addBlock(block) {
-    block.init(this, this.gl);
-    this.blocks.push(block);
+  IField.prototype.add = function add(item) {
+    item.init(this);
+
+    // Fast case
+    if (this.items.length === 0) this.items.push(item);
+
+    // Binary-search + insertion
+    var i = 0,
+        j = this.items.length - 1,
+        middle = 0;
+
+    while (i <= j) {
+      middle = (i + j) >> 1;
+      var cmp = Item.compare(item, this.items[middle]);
+
+      if (cmp == 0) {
+        break;
+      } else if (cmp < 0) {
+        j = middle - 1;
+      } else {
+        i = middle + 1;
+      }
+    }
+
+    if (cmp > 0) {
+      middle++;
+    }
+
+    // Insert
+    this.items.splice(middle, 0, item);
   };
 
-  function Block(x, y, z, kind) {
-    this.width = 50;
-
+  function Item(x, y, z) {
     this.x = x;
     this.y = y;
     this.z = z;
-
-    this.vertices = null;
-    this.colors = null;
-
+    this._x = null;
+    this._y = null;
+    this._z = null;
+    this.point = null;
     this.field = null;
+  };
+
+  Item.compare = function compare(a, b) {
+    var ax = a.x | 0,
+        ay = a.y | 0,
+        az = a.z | 0,
+        bx = b.x | 0,
+        by = b.y | 0,
+        bz = b.z | 0;
+
+    if (az > bz) return -1;
+    if (az < bz) return 1;
+
+    return ax - bx + ay - by;
+  };
+
+  Item.prototype.init = function init(field) {
+    this.field = field;
+  };
+
+  Item.prototype.render = function render(ctx) {
+    // Use cached projection if not changed
+    if (this._x !== this.x || this._y !== this.y || this._z !== this.z) {
+      this.point = this.field.project(this.x, this.y, this.z);
+      this._x = this.x;
+      this._y = this.y;
+      this._z = this.z;
+    }
+  };
+
+  function Block(x, y, z, kind) {
+    Item.call(this, x, y, z);
+
+    this.width = 50;
+
+    this.type = 'block';
     this.kind = kind;
   };
+  inherits(Block, Item);
+
   exports.Block = Block;
   exports.createBlock = function createBlock(x, y, z, kind) {
     return new Block(x, y, z, kind);
   };
 
-  Block.prototype.init = function init(field, gl) {
-    this.field = field;
+  Block.prototype.init = function init(field) {
+    Item.prototype.init.call(this, field);
     this.image = field.images[this.kind] || field.images.block;
   };
 
-  Block.prototype.render = function render(ctx, field) {
-    var p = field.project(this.x, this.y, this.z);
+  Block.prototype.render = function render(ctx) {
+    Item.prototype.render.call(this, ctx);
 
-    this.z += (Math.random() - 0.49999999) / 42;
     ctx.drawImage(this.image.elem,
-                  p.x - 32,
-                  p.y - 8,
+                  this.point.x - 32,
+                  this.point.y - 8,
                   this.image.width,
                   this.image.height);
+  };
+
+  function Player(x, y, z) {
+    Item.call(this, x, y, z);
+    this.type = 'player';
+    this.state = 'normal';
+
+    this.moving = false;
+  };
+  inherits(Player, Item);
+
+  exports.Player = Player;
+  exports.createPlayer = function createPlayer(x, y, z) {
+    return new Player(x, y, z);
+  };
+
+  Player.prototype.init = function init(field) {
+    Item.prototype.init.call(this, field);
+
+    this.images = {
+      normal: field.images.player,
+      flying: field.images['player-flying']
+    };
+  };
+
+  function animate(fn, times, interval, cb) {
+    function run() {
+      fn();
+      if (--times > 0) return setTimeout(run, interval);
+      cb();
+    }
+    run();
+  };
+
+  Player.prototype.move = function move(dx, dy, dz, cb) {
+    var self = this;
+
+    if (this.moving) {
+      return;
+    }
+    this.moving = true;
+
+    this.state = 'flying';
+    animate(function() {
+      self.z -= 0.03;
+    }, 6, 15, function() {
+      var times = 6;
+      animate(function() {
+        self.x += dx / times;
+        self.y += dy / times;
+        self.z += dz / times;
+      }, times, 15, function() {
+        self.state = 'normal';
+        animate(function() {
+          self.z += 0.06;
+        }, 3, 15, function() {
+          self.moving = false;
+
+          cb && cb();
+        });
+      });
+    });
+  };
+
+  Player.prototype.render = function render(ctx) {
+    Item.prototype.render.call(this, ctx);
+
+    ctx.drawImage(this.images[this.state].elem,
+                  this.point.x - 32,
+                  this.point.y - 32,
+                  64,
+                  64);
   };
 
   return exports;
